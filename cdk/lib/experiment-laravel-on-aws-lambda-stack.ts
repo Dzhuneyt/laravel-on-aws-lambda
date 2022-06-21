61bc3c8f22f281119e7aaaccfc04fd121f050602
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2022-06-21 14:05:00 +0300

    chore: Initial Laravel project

--- cdk/lib/experiment-laravel-on-aws-lambda-stack.ts
+++ cdk/lib/experiment-laravel-on-aws-lambda-stack.ts
@@ -1,16 +1,9 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Stack, StackProps} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
 
 export class ExperimentLaravelOnAwsLambdaStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ExperimentLaravelOnAwsLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    }
 }
