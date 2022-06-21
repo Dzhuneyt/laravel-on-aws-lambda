c1db8fec81a7802a03abbc770428fc765d9f4660
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2022-06-21 13:54:46 +0300

    chore: Initial CDK project

---
+++ cdk/test/experiment-laravel-on-aws-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ExperimentLaravelOnAwsLambda from '../lib/experiment-laravel-on-aws-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/experiment-laravel-on-aws-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ExperimentLaravelOnAwsLambda.ExperimentLaravelOnAwsLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
